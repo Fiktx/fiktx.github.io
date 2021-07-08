6a0ee87c96db625ef3f2d2f0760668177fb42aa8
Fik <fik.8ovce@gmail.com>
2021-07-08 18:25:23 +0200

    CSS Fix update v1.0.1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    if(navigator.userAgent.match(/Mobile|Windows Phone|Lumia|Android|webOS|iPhone|iPod|Blackberry|PlayBook|BB10|Opera Mini|\bCrMo\/|Opera Mobi/i)) window.location.href = "/device-not-supported/";
+    if(navigator.userAgent.match(/Mobile|Windows Phone|Lumia|webOS|iPhone|iPod|Blackberry|PlayBook|BB10|Opera Mini|\bCrMo\/|Opera Mobi/i)) window.location.href = "/device-not-supported/";
     //Buttons
     document.getElementById('footer-btt-button').onclick = () => window.scroll({top: 0,behavior: 'smooth'});
     document.getElementById('home-button').onclick = () => window.location.href = '/';
@@ -14,5 +14,5 @@ window.onload = function () {
     setTimeout(() => {type2();}, 2000);
     function type() {var text1=" Good ";if (i < text1.length) {document.getElementById('typing-animation1').innerHTML += text1.charAt(i);i++;setTimeout(type, 150);}};
     function untype() {document.getElementById('typing-animation1').style = "color: #FF0000;";setTimeout(() => {document.getElementById('typing-animation1').innerHTML = "";i = 0;document.getElementById('typing-animation1').style = "color: #00FF00;";}, 750);};
-    function type2() { var text2=" Best programmer "; if (i < text2.length) { document.getElementById('typing-animation1').innerHTML += text2.charAt(i); i++;setTimeout(type2, 150);} else {document.getElementById('typing-animation1').style = "color: #FFFFFF;";}};
+    function type2() { var text2=" Best programmer"; if (i < text2.length) { document.getElementById('typing-animation1').innerHTML += text2.charAt(i); i++;setTimeout(type2, 150);} else {document.getElementById('typing-animation1').style = "color: #FFFFFF;";}};
 }
